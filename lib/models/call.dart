cf3cbe6c318cfdcf251c29a02a9047c26faf7dfd
junedhayko502 <junedhayko502@gmail.com>
2024-03-17 12:51:01 +0000

    initial commit

---
+++ lib/models/call.dart
@@ -0,0 +1,12 @@
+class Call{
+  String name;
+  String callDate;
+
+  Call({required this.name, required this.callDate});
+}
+
+var callList = [
+  Call(name: "Pak Gun", callDate: "4 March, 10:48 PM"),
+  Call(name: "Pak Bambang", callDate: "5 March, 10:30 AM"),
+  Call(name: "Pak Hendro", callDate: "6 March, 10:31 AM"),
+];
---
+++ lib/models/status.dart
@@ -0,0 +1,12 @@
+class Status{
+  String name;
+  String statusDate;
+
+  Status({required this.name, required this.statusDate});
+}
+
+var statusList = [
+  Status(name: "Jnd", statusDate: "Now, 10:48 PM"),
+  Status(name: "Adm", statusDate: "Now, 10:30 AM"),
+  Status(name: "Rmd", statusDate: "Now, 10:31 AM"),
+];
